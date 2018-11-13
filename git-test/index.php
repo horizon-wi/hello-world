be49008d8af7c3ed1df2e6c7f1aa397ad1ad5a6c
rsmith <rsmith@WIABAPC45.wiaba.local>
2018-11-13 13:22:05 -0600

    ide content

--- git-test/index.php
+++ git-test/index.php
@@ -9,5 +9,6 @@
         <p>Lu til resera mun li dabase joce deh miemela rel! Cicepod cacari iesusi!</p>
         <p>test</p>
         <p>from cmd - development branch</p>
+        <p>update from IDE</p>
     </body>
 </html>
